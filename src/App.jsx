9f38a8c50c1a585e6400fb2a368808e7559ac79d
Djibril-Mugisho <mugishodjibril2004@gmail>
2023-07-22 04:48:01 +0300

    fix linters error

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Routes, Route } from 'react-router';
 import Header from './components/Header';
 import React from 'react';
 import Books from './screens/Books';
-import Form from './components/Form';
+
 
 function App() {
   return (
