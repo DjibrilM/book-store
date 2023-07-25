6d798b20826fe7dd68de72506ead799ea6f30c7f
Djibril-Mugisho <mugishodjibril2004@gmail>
2023-07-25 20:08:04 +0300

    improved folder structure

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import booksSlice from "./features/books/booksSlice";
-import categoriesSlice from "./features/categories/categoriesSlice";
+import booksSlice from "./books/booksSlice";
+import categoriesSlice from "./categories/categoriesSlice";
 
 const store = configureStore({
     reducer: {
